5f19fce3572323108651015953ab47837348b51b
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-11-25 20:36:42 +0000

    fix

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'calabash-cucumber', '0.16.4'
+gem 'calabash-android', '0.5.15'
+gem 'xamarin-test-cloud', '1.1.2'
--- step.sh
+++ step.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
-ruby "${THIS_SCRIPTDIR}/step.rb" \
+current_path=$(pwd)
+cd $THIS_SCRIPT_DIR
+bundle exec ruby "step.rb" \
 	-t "${calabash_features}" \
 	-d "${simulator_device}" \
 	-o "${simulator_os_version}"
+cd $current_path

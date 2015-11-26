cbbe0238a4214563a31cab6ed31427a642728301
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-11-26 14:50:32 +0000

    test

--- step.sh
+++ step.sh
@@ -4,15 +4,15 @@ this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
-# current_path=$(pwd)
-# cd $this_script_dir
-# bundle exec ruby "step.rb" \
-# 	-a "${calabash_features}" \
-# 	-b "${simulator_device}" \
-# 	-c "${simulator_os_version}"
-# cd $current_path
-
-ruby "$this_script_dir/step.rb" \
+current_path=$(pwd)
+cd $this_script_dir
+bundle exec ruby "step.rb" \
 	-a "${calabash_features}" \
 	-b "${simulator_device}" \
 	-c "${simulator_os_version}"
+cd $current_path
+
+# ruby "$this_script_dir/step.rb" \
+# 	-a "${calabash_features}" \
+# 	-b "${simulator_device}" \
+# 	-c "${simulator_os_version}"
